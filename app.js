94c487a4427348379f43f8fc799f36f96dbcc693
Dun2901 <lamchidung373@gmail.com>
2025-05-27 00:20:23 +0700

    vid 24

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log("Hello World");
-console.log("Hello hoidanit");
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.get("/kophaidun", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+    console.log(`My app is running on port ${PORT}`);
+})
