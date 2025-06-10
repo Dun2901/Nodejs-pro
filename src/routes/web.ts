e5512f148395a62243df90f96ed7e590af9a033e
Dun2901 <lamchidung373@gmail.com>
2025-06-11 00:39:23 +0700

    vid 34

---
+++ src/routes/web.ts
@@ -0,0 +1,18 @@
+import express, { Express } from "express";
+
+const router = express.Router();
+
+const webRoutes = (app: Express) => {
+    router.get("/", (req, res) => {
+        res.render("home.ejs")
+    });
+
+    router.get("/kophaidun", (req, res) => {
+        res.send("Hello World")
+    });
+
+    app.use("/", router);
+};
+
+export default webRoutes;
+
