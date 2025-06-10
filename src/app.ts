9b63bfe439d449241964247d83b8dafb98e5c6a2
Dun2901 <lamchidung373@gmail.com>
2025-06-11 00:14:28 +0700

    vid 32

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-// const express = require("express");
 import express from "express";
 import 'dotenv/config'
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get("/", (req, res) => {
-    res.send("Hello World nodemon")
+    res.render("home.ejs")
 })
 
 app.get("/kophaidun", (req, res) => {
@@ -15,5 +18,4 @@ app.get("/kophaidun", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`My app is running on port ${PORT}`);
-    console.log("env port: ", process.env.PORT);
 })
