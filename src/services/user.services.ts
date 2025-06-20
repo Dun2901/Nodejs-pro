8d2ce481870d133bf2c31a8627ee42998add2214
Dun2901 <lamchidung373@gmail.com>
2025-06-20 13:01:10 +0700

    vid 48

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,10 +1,17 @@
 import getConnection from "../config/db";
 
-const handleCreateUser = (fullName: string, email: string, address: string) => {
+const handleCreateUser = async (fullName: string, email: string, address: string) => {
     // insert into database
+    const connection = await getConnection();
 
-    // return result
-    console.log(">>> insert a new user");
+    try {
+        const sql = 'INSERT INTO `users`(`name`, `email`,`address`) VALUES (?, ?, ?)';
+        const values = [fullName, email, address];
+        const [result, fields] = await connection.execute(sql, values);
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
 };
 
 const getAllUsers = async () => {
