c44a20d6c9971f452f61f311c0fd1fb2216dcbda
Dun2901 <lamchidung373@gmail.com>
2025-08-15 01:43:41 +0700

    vid 84

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,7 +1,11 @@
 import { ACCOUNT_TYPE } from 'config/constant';
 import { prisma } from "config/client";
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
 
-
+const hashPassword = async (plainText: string) => {
+    return await bcrypt.hash(plainText, saltRounds);
+};
 const handleCreateUser = async (
     fullName: string,
     email: string,
@@ -9,12 +13,13 @@ const handleCreateUser = async (
     phone: string,
     avatar: string
 ) => {
+    const defaultPassword = await hashPassword("123456");
     const newUser = await prisma.user.create({
         data: {
             fullName: fullName,
             username: email,
             address: address,
-            password: "123456",
+            password: defaultPassword,
             accountType: ACCOUNT_TYPE.SYSTEM,
             avatar: avatar,
             phone: phone,
@@ -62,5 +67,5 @@ const updateUserById = async (id: string, email: string, address: string, fullNa
 
 export {
     handleCreateUser, getAllUsers, handleDeleteUser,
-    getUserById, updateUserById, getAllRoles
+    getUserById, updateUserById, getAllRoles, hashPassword
 };
