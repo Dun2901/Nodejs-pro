f14d043b97bf07d6babdffba401775aae8f77cfc
Dun2901 <lamchidung373@gmail.com>
2025-10-13 00:24:50 +0700

    vid 97

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,10 +1,39 @@
 import { Request, Response } from "express";
+import { ProductSchema, TProductSchema } from "src/validation/product.schema";
 
 const getAdminCreateProductPage = (req: Request, res: Response) => {
-    return res.render("admin/product/create.ejs");
+    const errors = [];
+    const oldData = {
+        name: "",
+        price: "",
+        detailDesc: "",
+        shortDesc: "",
+        quantity: "",
+        factory: "",
+        target: "",
+    };
+    return res.render("admin/product/create.ejs", {
+        errors, oldData
+    });
 };
 const postAdminCreateProduct = (req: Request, res: Response) => {
-    const { name } = req.body;
+    const { name, price, detailDesc, shortDesc, quantity, factory, target } = req.body as TProductSchema;
+
+    const validate = ProductSchema.safeParse(req.body);
+
+    if (!validate.success) {
+        // error
+        const errorsZod = validate.error.issues;
+        const errors = errorsZod?.map(item => `${item.message} (${item.path[0]})`);
+        const oldData = {
+            name, price, detailDesc, shortDesc, quantity, factory, target
+        };
+        return res.render("admin/product/create.ejs", {
+            errors, oldData
+        });
+    }
+
+    //succes
     return res.redirect("/admin/product");
 };
 
