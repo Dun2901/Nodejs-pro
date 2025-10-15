71566366babe475ab25af5b1b17f63e924606232
Dun2901 <lamchidung373@gmail.com>
2025-10-15 23:57:27 +0700

    vid 99

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createProduct } from "services/admin/product.service";
+import { createProduct, getProductById, getProductList, handleDeleteProduct, updateProductById } from "services/admin/product.service";
 import { ProductSchema, TProductSchema } from "src/validation/product.schema";
 
 const getAdminCreateProductPage = (req: Request, res: Response) => {
@@ -40,7 +40,53 @@ const postAdminCreateProduct = async (req: Request, res: Response) => {
     );
     return res.redirect("/admin/product");
 };
+const postDeleteProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await handleDeleteProduct(+id);
+    return res.redirect("/admin/product");
+};
+const getViewProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // Get product by id
+    const product = await getProductById(+id);
+
+    const factoryOptions = [
+        { name: "Apple (MacBook)", value: "APPLE" },
+        { name: "Asus", value: "ASUS" },
+        { name: "Lenovo", value: "LENOVO" },
+        { name: "Dell", value: "DELL" },
+        { name: "LG", value: "LG" },
+        { name: "Acer", value: "ACER" },
+    ];
+    const targetOptions = [
+        { name: "Gaming", value: "GAMING" },
+        { name: "Sinh viên - Văn phòng", value: "SINHVIEN-VANPHONG" },
+        { name: "Thiết kế đồ họa", value: "THIET-KE-DO-HOA" },
+        { name: "Mỏng nhẹ", value: "MONG-NHE" },
+        { name: "Doanh nhân", value: "DOANH-NHAN" },
+    ];
+
+    return res.render("admin/product/detail.ejs", {
+        product,
+        factoryOptions,
+        targetOptions
+    });
+};
+const postUpdateProduct = async (req: Request, res: Response) => {
+    const {
+        id,
+        name, price, detailDesc, shortDesc,
+        quantity, factory, target
+    } = req.body as TProductSchema;
+    const image = req?.file?.filename ?? null;
+    await updateProductById(
+        +id, name, +price, detailDesc, shortDesc,
+        +quantity, factory, target, image
+    );
+    return res.redirect("/admin/product");
+};
 
 export {
-    getAdminCreateProductPage, postAdminCreateProduct
+    getAdminCreateProductPage, postAdminCreateProduct, postDeleteProduct,
+    getViewProduct, postUpdateProduct
 };
--- src/services/admin/product.service.ts
+++ src/services/admin/product.service.ts
@@ -26,7 +26,44 @@ const createProduct = async (
 const getProductList = async () => {
     return await prisma.product.findMany();
 };
+const handleDeleteProduct = async (id: number) => {
+    await prisma.product.delete({
+        where: { id: id }
+    });
+};
+const getProductById = async (id: number) => {
+    return await prisma.product.findUnique({
+        where: { id }
+    });
+};
+const updateProductById = async (
+    id: number,
+    name: string,
+    price: number,
+    detailDesc: string,
+    shortDesc: string,
+    quantity: number,
+    factory: string,
+    target: string,
+    imageUpload: string
+) => {
+    await prisma.product.update({
+        where: { id },
+        data: {
+            id,
+            name,
+            price,
+            detailDesc,
+            shortDesc,
+            quantity,
+            factory,
+            target,
+            ...(imageUpload && { image: imageUpload })
+        }
+    });
+};
 
 export {
-    createProduct, getProductList
+    createProduct, getProductList, handleDeleteProduct,
+    getProductById, updateProductById
 }
