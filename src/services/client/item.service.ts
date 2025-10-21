e005863afd77b1d0d35ec361d08e458d9be76e4b
Dun2901 <lamchidung373@gmail.com>
2025-10-21 21:27:29 +0700

    vid 101

--- src/services/client/item.service.ts
+++ src/services/client/item.service.ts
@@ -4,7 +4,12 @@ const getProducts = async () => {
     const products = await prisma.product.findMany();
     return products;
 };
+const getProductById = async (id: number) => {
+    return await prisma.product.findUnique({
+        where: { id }
+    });
+};
 
 export {
-    getProducts
+    getProducts, getProductById
 };
