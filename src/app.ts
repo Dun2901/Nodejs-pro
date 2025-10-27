279e14d01dfd2891950b1c26785cd0bc60860136
Dun2901 <lamchidung373@gmail.com>
2025-10-27 13:15:48 +0700

    vid 105

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
 import webRoutes from "./routes/web";
 import initDatabase from "config/seed";
 
@@ -7,15 +7,15 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // config view engine
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 // config req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // config static files: images/css/js
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // config routes
 webRoutes(app);
@@ -23,6 +23,11 @@ webRoutes(app);
 // seeding data
 initDatabase();
 
+// Handle 404 not found
+app.use((req, res) => {
+  res.send("404 not found");
+});
+
 app.listen(PORT, () => {
-    console.log(`My app is running on port ${PORT}`);
-})
+  console.log(`My app is running on port ${PORT}`);
+});
