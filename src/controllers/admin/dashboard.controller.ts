4a04f57ab6792aaa57aed2de1bd0e0334d779e4a
Dun2901 <lamchidung373@gmail.com>
2025-12-01 00:12:05 +0700

    vid 140

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { getDashboardInfo } from "services/admin/dashboard.service";
-import { getOrderAdmin, getOrderDetailAdmin } from "services/admin/order.service";
-import { getProductList } from "services/admin/product.service";
+import {
+  countTotalOrderPages,
+  getOrderAdmin,
+  getOrderDetailAdmin,
+} from "services/admin/order.service";
+import { countTotalProductPages, getProductList } from "services/admin/product.service";
 import { countTotalUserPages, getAllUsers } from "services/user.service";
 
 const getDashboardPage = async (req: Request, res: Response) => {
@@ -19,24 +23,43 @@ const getAdminUserPage = async (req: Request, res: Response) => {
 
   const users = await getAllUsers(currentPage);
   const totalPages = await countTotalUserPages();
+  console.log("totalPages:", totalPages);
 
   return res.render("admin/user/show.ejs", {
     users,
     totalPages: +totalPages,
-    page: +page,
+    page: +currentPage,
   });
 };
 
 const getAdminProductPage = async (req: Request, res: Response) => {
-  const products = await getProductList();
+  const { page } = req.query;
+
+  let currentPage = page ? +page : 1;
+  if (currentPage <= 0) currentPage = 1;
+
+  const totalPages = await countTotalProductPages();
+  const products = await getProductList(currentPage);
   return res.render("admin/product/show.ejs", {
     products,
+    totalPages: +totalPages,
+    page: +currentPage,
   });
 };
 
 const getAdminOrderPage = async (req: Request, res: Response) => {
-  const orders = await getOrderAdmin();
-  return res.render("admin/order/show.ejs", { orders });
+  const { page } = req.query;
+
+  let currentPage = page ? +page : 1;
+  if (currentPage <= 0) currentPage = 1;
+
+  const totalPages = await countTotalOrderPages();
+  const orders = await getOrderAdmin(currentPage);
+  return res.render("admin/order/show.ejs", {
+    orders,
+    totalPages: +totalPages,
+    page: +currentPage,
+  });
 };
 
 const getAdminOrderDetailPage = async (req: Request, res: Response) => {
--- src/services/admin/product.service.ts
+++ src/services/admin/product.service.ts
@@ -1,69 +1,88 @@
 import { prisma } from "config/client";
+import { TOTAL_ITEMS_PER_PAGE } from "config/constant";
 
 const createProduct = async (
-    name: string,
-    price: number,
-    detailDesc: string,
-    shortDesc: string,
-    quantity: number,
-    factory: string,
-    target: string,
-    imageUpload: string
+  name: string,
+  price: number,
+  detailDesc: string,
+  shortDesc: string,
+  quantity: number,
+  factory: string,
+  target: string,
+  imageUpload: string,
 ) => {
-    await prisma.product.create({
-        data: {
-            name,
-            price,
-            detailDesc,
-            shortDesc,
-            quantity,
-            factory,
-            target,
-            ...(imageUpload && { image: imageUpload })
-        }
-    })
+  await prisma.product.create({
+    data: {
+      name,
+      price,
+      detailDesc,
+      shortDesc,
+      quantity,
+      factory,
+      target,
+      ...(imageUpload && { image: imageUpload }),
+    },
+  });
 };
-const getProductList = async () => {
-    return await prisma.product.findMany();
+const getProductList = async (page: number) => {
+  const pageSize = TOTAL_ITEMS_PER_PAGE;
+  const skip = (page - 1) * pageSize;
+  return await prisma.product.findMany({
+    skip: skip,
+    take: pageSize,
+  });
 };
+
+const countTotalProductPages = async () => {
+  const pageSize = TOTAL_ITEMS_PER_PAGE;
+  const totalItems = await prisma.product.count();
+
+  const totalPages = Math.ceil(totalItems / pageSize);
+  return totalPages;
+};
+
 const handleDeleteProduct = async (id: number) => {
-    await prisma.product.delete({
-        where: { id: id }
-    });
+  await prisma.product.delete({
+    where: { id: id },
+  });
 };
 const getProductById = async (id: number) => {
-    return await prisma.product.findUnique({
-        where: { id }
-    });
+  return await prisma.product.findUnique({
+    where: { id },
+  });
 };
 const updateProductById = async (
-    id: number,
-    name: string,
-    price: number,
-    detailDesc: string,
-    shortDesc: string,
-    quantity: number,
-    factory: string,
-    target: string,
-    imageUpload: string
+  id: number,
+  name: string,
+  price: number,
+  detailDesc: string,
+  shortDesc: string,
+  quantity: number,
+  factory: string,
+  target: string,
+  imageUpload: string,
 ) => {
-    await prisma.product.update({
-        where: { id },
-        data: {
-            id,
-            name,
-            price,
-            detailDesc,
-            shortDesc,
-            quantity,
-            factory,
-            target,
-            ...(imageUpload && { image: imageUpload })
-        }
-    });
+  await prisma.product.update({
+    where: { id },
+    data: {
+      id,
+      name,
+      price,
+      detailDesc,
+      shortDesc,
+      quantity,
+      factory,
+      target,
+      ...(imageUpload && { image: imageUpload }),
+    },
+  });
 };
 
 export {
-    createProduct, getProductList, handleDeleteProduct,
-    getProductById, updateProductById
-}
+  createProduct,
+  getProductList,
+  handleDeleteProduct,
+  getProductById,
+  updateProductById,
+  countTotalProductPages,
+};
