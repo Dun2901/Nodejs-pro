c98b4d67cc18a69094f3de3ad9c074383d3b6c1f
Dun2901 <lamchidung373@gmail.com>
2025-12-13 00:14:16 +0700

    vid 174

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,17 +8,19 @@ import {
   updateUserByIdAPI,
 } from "controllers/client/api.controller";
 import express, { Express } from "express";
+import { checkValidJWT } from "src/middleware/jwt.middleware";
 
 const router = express.Router();
 
 const apiRoutes = (app: Express) => {
   router.post("/add-product-to-cart", postAddProductToCartAPI);
 
-  router.get("/users", getAllUsersAPI);
+  router.get("/users", checkValidJWT, getAllUsersAPI);
   router.get("/users/:id", getUserByIdAPI);
   router.post("/users", createUsersAPI);
   router.put("/users/:id", updateUserByIdAPI);
   router.delete("/users/:id", deleteUserByIdAPI);
+
   router.post("/login", loginAPI);
 
   app.use("/api", router);
--- src/services/client/api.service.ts
+++ src/services/client/api.service.ts
@@ -1,6 +1,7 @@
 import { prisma } from "config/client";
 import { comparePassword } from "services/user.service";
 import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const handleGetAllUser = async () => {
   return await prisma.user.findMany();
@@ -54,10 +55,13 @@ const handleUserLogin = async (username: string, password: string) => {
 
   // Có user login => định nghĩa access token
   const payload = {
-    id: 1,
-    name: "hoidanit",
+    id: user.id,
+    username: user.username,
+    roleId: user.roleId,
   };
-  const access_token = jwt.sign(payload, "eric", {
+  const secret = process.env.JWT_SECRET;
+
+  const access_token = jwt.sign(payload, secret, {
     expiresIn: "1d",
   });
 
