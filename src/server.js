84ef28c618edc00d0414d57c95b57d076b9f54c5
Dun2901 <lamchidung373@gmail.com>
2025-12-22 16:57:40 +0700

    vid 18

--- src/server.js
+++ src/server.js
@@ -4,8 +4,18 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+// config view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// route
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/test", (req, res) => {
+  // res.send("Hello World!");
+  res.render("sample.ejs");
 });
 
 app.listen(port, () => {
