110c91663a3bd5a57f873bed39edd2ae5fc2991d
Dun2901 <lamchidung373@gmail.com>
2025-12-28 22:29:05 +0700

    vid 30

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 require("dotenv").config();
+const mysql = require("mysql2");
 const webRoutes = require("./routes/web");
 
 const app = express();
@@ -12,6 +13,20 @@ configViewEngine(app);
 
 // route
 app.use("/", webRoutes);
+
+// test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+  console.log("results", results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
