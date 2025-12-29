e21a27ce7f92b8cfb871f0111f7f7b6ccee9e387
Dun2901 <lamchidung373@gmail.com>
2025-12-29 14:05:35 +0700

    vid 35

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,5 +1,7 @@
+const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
-  res.send("Hello World and nodemon");
+  return res.render("home.ejs");
 };
 
 module.exports = {
