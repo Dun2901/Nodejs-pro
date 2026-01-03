32ce2baddd0e26c574bf34a49b43b9e7f85c10eb
Dun2901 <lamchidung373@gmail.com>
2026-01-04 00:01:12 +0700

    vid 76

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -24,7 +24,19 @@ const postCreateUserAPI = async (req, res) => {
   });
 };
 
+const putUpdateUserAPI = async (req, res) => {
+  const { email, name, city, id } = req.body;
+
+  const user = await User.updateOne({ _id: id }, { email, name, city });
+
+  return res.status(200).json({
+    EC: 0,
+    data: user,
+  });
+};
+
 module.exports = {
   getUsersAPI,
   postCreateUserAPI,
+  putUpdateUserAPI,
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getUsersAPI, postCreateUserAPI } = require("../controllers/api.controller");
+const {
+  getUsersAPI,
+  postCreateUserAPI,
+  putUpdateUserAPI,
+} = require("../controllers/api.controller");
 const routerAPI = express.Router();
 
 routerAPI.get("/users", getUsersAPI);
 routerAPI.post("/users", postCreateUserAPI);
+routerAPI.put("/users", putUpdateUserAPI);
 
 module.exports = routerAPI; // export default
