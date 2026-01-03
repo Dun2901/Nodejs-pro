0861e49d890d0ea0386df1d7274424b8f94ed50e
Dun2901 <lamchidung373@gmail.com>
2026-01-03 23:50:41 +0700

    vid 74

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,17 +1,8 @@
 const express = require("express");
-const { getUsersAPI } = require("../controllers/api.controller");
+const { getUsersAPI, postCreateUserAPI } = require("../controllers/api.controller");
 const routerAPI = express.Router();
 
-routerAPI.get("/", (req, res) => {
-  res.send("hello world with api");
-});
-
-routerAPI.get("/abc", (req, res) => {
-  res.status(200).json({
-    data: "hello world first apis",
-  });
-});
-
 routerAPI.get("/users", getUsersAPI);
+routerAPI.post("/users", postCreateUserAPI);
 
 module.exports = routerAPI; // export default
