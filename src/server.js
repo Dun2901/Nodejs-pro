d3da76fbfa20bbb33e51d573e7a828fa75cd695b
Dun2901 <lamchidung373@gmail.com>
2026-01-03 14:44:17 +0700

    vid 60

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const configViewEngine = require("./config/viewEngine");
 require("dotenv").config();
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
-const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -19,11 +18,6 @@ configViewEngine(app);
 // route
 app.use("/", webRoutes);
 
-const kittySchema = new mongoose.Schema({
-  name: String,
-});
-
-const Kitten = mongoose.model("Kitten", kittySchema);
 const cat = new Kitten({ name: "dung cat" });
 cat.save();
 
